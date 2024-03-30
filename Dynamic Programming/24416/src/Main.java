8c2a707ee36c1e783cd0efdd9397c338fa3b97e8
LeeWooJung <wjlee612@gmail.com>
2024-03-31 00:59:37 +0900

    Dynamic Programming

---
+++ Dynamic Programming/24416/src/Main.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception, IOException {
+        
+        // 변수 설정
+        int n;
+        int[] dp;
+
+        // BufferedReader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 변수 입력
+        n = Integer.parseInt(br.readLine());
+        dp = new int[n+1];
+
+        // 문제 해결
+        dp[1] = dp[2] = 1;
+
+        for(int i = 3; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        System.out.println(dp[n] + " " + (n-2));
+
+        br.close();
+    }
+
+}
