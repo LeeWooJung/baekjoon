d773eb3ba40cba91e1da5928012e6d9e88a67cd2
LeeWooJung <wjlee612@gmail.com>
2024-03-31 20:46:54 +0900

    Dynamic Programming

---
+++ Dynamic Programming/2579/src/Main.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception, IOException {
+        
+        // 변수 설정
+        int nStairs;
+        int[] Stair = new int[301]; 
+        long[] dp = new long[301];
+
+        // BufferedReader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 변수 입력
+        nStairs = Integer.parseInt(br.readLine());
+
+        for(int step = 1; step <= nStairs; step++) {
+            Stair[step] = Integer.parseInt(br.readLine());
+        }
+
+        // Dynamic Programming
+        for(int step = 1; step <= nStairs; step++) {
+            if(step >= 3) {
+                dp[step] = Stair[step] + Math.max(Stair[step-1] + dp[step-3], dp[step-2]);
+            } else {
+                dp[step] = Stair[step] + dp[step-1];
+            }
+        }
+        
+        System.out.println(dp[nStairs]);
+        br.close();
+    }
+}
